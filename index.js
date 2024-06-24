8581f95564ed715c594648006c8d805122905372
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-06-24 19:34:30 +0300

    Last Commit

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const button = document.getElementById('button');
+    button.addEventListener('click', function() {
+        alert('Button has been clicked!');
+    }); 
 }
